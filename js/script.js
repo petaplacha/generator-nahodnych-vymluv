29b7746dc61a2c69642f7fbe4e7e5ec78d1c16a4
petaplacha <petaplacha2000@gmail.com>
2019-07-17 14:00:50 +0200

    generovani vymluv 2. cast
    dodelani funkce, vzhledova uprava

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ function generovatVymluvu() {
         'February', 
         'March'];    
     var rand = vymluvy[Math.floor(Math.random() * vymluvy.length)];
+    var element = document.getElementById("generovane");
+    element.classList.remove("generovane");
     document.getElementById("vymluva").innerHTML = rand;
 }
